b7d2b95129c4ce5667285b1dfffc34d8863e1c89
Nick Novitski <nicknovitski@gmail.com>
2015-02-19 07:26:21 +0000

    move language dependency to dev profile and remove environ

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 (defproject steam "0.0.1"
-  :description "idiomatic client to the Steam APIs, built with schema reflection"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.3"]
-                 [org.clojure/data.xml "0.0.7"]
-                 [clj-http "0.7.8"]
-                 [environ "0.2.1"]
+  :description "idiomatic client to the Steam APIs"
+  :dependencies [[org.clojure/data.json "0.2.5"]
+                 [org.clojure/data.xml "0.0.8"]
+                 [clj-http "1.0.1"]
                  [ring/ring-codec "1.0.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.0"]]}})
-
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+                                  [midje "1.6.0"]]}})
