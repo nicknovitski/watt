85fdbe9c61282f00cca26c2728e2f8600b6e2225
Nick Novitski <nicknovitski@gmail.com>
2015-02-20 10:49:11 +0000

    name->symbol correctly hyphenates interface names

--- src/steam/build.clj
+++ src/steam/build.clj
@@ -9,26 +9,14 @@
 (defn- decapitalize [string]
   (str/replace-first string #"^." str/lower-case))
 
-(defn- insert-hyphens [string]
-  (let [lower-case (str/lower-case string)
-        length (count string)]
-    (str
-      \-
-      (if (< length 3)
-        lower-case
-        (let [last-character (subs lower-case (dec length))
-              all-but-last-character (subs lower-case 0 (dec length))]
-          (str all-but-last-character \- last-character))))))
+(defn- words [string]
+  (map first (re-seq #"(([A-Z]([A-Z]+|[a-z]+)[0-9]?)|[0-9]+)(?![a-z])" string)))
 
 (defn- hyphenate [string]
-  (let [any-capitals #"[A-Z]+"]
-    (str/replace
-      (str/replace string "_" "-")
-      any-capitals
-      insert-hyphens)))
+  (str/join "-" (words string)))
 
 (defn name->symbol [string]
-  (-> string remove-prefixes remove-suffixes decapitalize hyphenate symbol))
+  (-> string remove-prefixes remove-suffixes hyphenate .toLowerCase symbol))
 
 (defn name->ns [string]
   (list 'ns (symbol (str "steam." (name->symbol string)))
