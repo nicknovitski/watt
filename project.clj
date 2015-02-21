36613b5ff04300ed7939a7394c4b7024c1efcf86
Nick Novitski <nicknovitski@gmail.com>
2015-02-21 08:53:16 +0000

    delete steam.util
    Turns out, steam.user/resolve-vanity-url did exactly what I needed.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject steam "0.0.1"
   :description "idiomatic client to the Steam APIs"
   :dependencies [[org.clojure/data.json "0.2.5"]
-                 [org.clojure/data.xml "0.0.8"]
                  [clj-http "1.0.1"]
                  [ring/ring-codec "1.0.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
