8cb5248f0ddf156bb9a87b93f67714bd34fd9130
Nick Novitski <nicknovitski@gmail.com>
2015-02-26 04:08:01 +0000

    license under apache 2.0

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject watt "0.0.1"
   :description "idiomatic client to the Steam APIs"
+  :url "https://github.com/nicknovitski/watt"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/data.json "0.2.5"]
                  [clj-http "1.0.1"]
                  [ring/ring-codec "1.0.0"]]
