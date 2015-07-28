7055919c6d95d9c235e85ca4fc12901254af0fc6
Nick Novitski <github@nicknovitski.com>
2015-07-27 18:04:08 -0700

    upgrade clj-http to 2.0.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/data.json "0.2.5"]
-                 [clj-http "1.0.1"]
+                 [clj-http "2.0.0"]
                  [ring/ring-codec "1.0.0"]]
   :plugins  [[lein-cljfmt "0.1.10"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}}
